cc3cf91eb236558d07bf90b9e2644f18c2f70d25
ashybot <amjacobs@ucsd.edu>
2022-11-21 18:40:14 -0800

    Added 10 enemies to UI and edited playership art

--- src/Model.hs
+++ src/Model.hs
@@ -8,9 +8,10 @@ type Name = ()
 data Tick = Tick
 
 -- Definition of types of cells in the game
-data Cell = EmptyCell | PlayershipCell
+data Cell = EmptyCell | PlayershipCell | EnemyCell
 type Coord = V2 Int
 type Playership = Seq Coord
+type Enemy = Seq Coord
 
 -- Definition of Game and Level attributes
 data Game = Game
@@ -19,6 +20,7 @@ data Game = Game
   , score       :: Int
   , dead        :: Bool
   , playership  :: Playership
+  , enemies     :: [Enemy]
   } deriving (Show)
 
 data Level = Level
@@ -34,7 +36,10 @@ game s li l = Game
         , score       = s
         , dead        = False
         , playership  = S.singleton (V2 (width `div` 2) 0)
+        , enemies = map S.singleton (enemyCoords 10)
         }
+enemyCoords :: Int -> [Coord]
+enemyCoords n = [V2 (((width `div` 2) + ((n*2) `div` 2)) - (x*2)) (height - 2) | x <- [1..n]]
 
 initGame :: IO Game
 initGame = do
