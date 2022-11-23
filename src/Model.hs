a19cfcb0b09e20c872f567c28ea47961efd18866
Gongyi Shi <goshi@ucsd.edu>
2022-11-23 13:27:20 -0800

    make enemy attack in order; add return-from-top logic once reach the end

--- src/Model.hs
+++ src/Model.hs
@@ -75,7 +75,7 @@ instance Ord Enemy where
 initEnemies:: Int -> Int -> Direction -> Enemies
 initEnemies n f d = Enemies (initEnemyList n d) f [] []
 initEnemyList:: Int -> Direction -> [Enemy]
-initEnemyList n d = [E (V2 (((width `div` 2) + ((n*2) `div` 2)) - (x*2)) (height - 2)) False d | x <- [1..n]]
+initEnemyList n d = [E (V2 (((width `div` 2) + ((n*2) `div` 2)) - (x*2)) enemyHeight) False d | x <- [1..n]]
 
 setAttackFrequency :: Int -> Int 
 setAttackFrequency = (100-)
@@ -111,23 +111,42 @@ pickNewAttackEnemy es@(Enemies el f op ae)
 -- Attack enemies move
 updateAttackMove :: Int -> Enemies -> Enemies
 updateAttackMove px es@(Enemies el f op ae)
-  = Enemies el f op (map (horizontalMove . moveEnemy D) ae)
+  = do 
+    -- put back attack enemy that arrives original patch
+    let idx = tryGetAttackEnemyByY ae 0 (enemyHeight+1)  
+    let (Enemies el' _ op' ae') = putBackAttackEnemy idx es
+    Enemies el' f op' (map (horizontalMove . moveEnemy D) ae')
     where 
-      horizontalMove e@(E (V2 ex _) _ _) = do 
-        if ex < px
-          then moveEnemy R e 
-          else if ex > px 
-            then moveEnemy L e 
-            else e 
+      horizontalMove e@(E (V2 ex ey) _ _) = do 
+        -- on the way back to original position
+        if ey-1 > enemyHeight
+          then moveEnemy D e 
+            -- on the way attacking player
+            else if ex < px
+              then moveEnemy R e 
+              else if ex > px 
+                then moveEnemy L e 
+                else e 
   
+-- Put back attack enemy to original position
+putBackAttackEnemy :: Int -> Enemies -> Enemies
+putBackAttackEnemy idx es@(Enemies el f op ae)
+  = if idx == -1 then es 
+      else do
+        let op_ = op!!idx
+        let el' = el ++ [op_]
+        let ae' = removeEnemy idx ae 
+        let op' = removeEnemy idx op 
+        Enemies el' f op' ae'
+
 -- Return the index of attack enemy at the bottom else -1
-tryGetBottomAttackEnemy :: [Enemy] -> Int -> Int
-tryGetBottomAttackEnemy [] _ = -1
-tryGetBottomAttackEnemy (a@(E (V2 _ y) _ _): as) idx 
+tryGetAttackEnemyByY :: [Enemy] -> Int -> Int -> Int
+tryGetAttackEnemyByY [] _ _ = -1
+tryGetAttackEnemyByY (a@(E (V2 _ y) _ _): as) idx y_val
   = do
-    if y <= 1
+    if y == y_val
       then idx
-      else tryGetBottomAttackEnemy as (idx+1)
+      else tryGetAttackEnemyByY as (idx+1) y_val
 
 -- Remove enemy from list by index 
 removeEnemy :: Int -> [Enemy] -> [Enemy]
@@ -138,15 +157,13 @@ removeEnemy idx el = lhs ++ rhs
 returnFinishedAttack :: Enemies -> Enemies
 returnFinishedAttack es@(Enemies el f op ae)
   = do
-    let idx = tryGetBottomAttackEnemy ae 0
+    let idx = tryGetAttackEnemyByY ae 0 1
     if idx == -1 
       then es
-      else do
-        let op_ = op!!idx
-        let el' = op_:el
-        let ae' = removeEnemy idx ae 
-        let op' = removeEnemy idx op 
-        Enemies el' f op' ae'
+      else do 
+        let (E (V2 x y) e d) = ae!!idx
+        let ae' = E (V2 x height) e d:ae
+        Enemies el f op ae'
 
 
 
@@ -186,9 +203,13 @@ initGame = do
             return $game 0 3 l
 
 getLevel :: Int -> Level
-getLevel n = Level {levelNumber = n, attackFrequency = 100 - 10*n}
+getLevel n = Level {levelNumber = n, attackFrequency = 30 - 10*n}
 
 -- Initialize screen size
 height, width :: Int
 height = 15
 width = 35
+
+-- Initialize enemy position
+enemyHeight :: Int 
+enemyHeight = height - 2
